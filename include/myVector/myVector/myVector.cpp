60b4179137b3c40d70cfffebdb1c93ffc04d7e55
ZachEBolden <zachebolden@gmail.com>
2023-08-28 17:18:15 -0500

    made a vector

---
+++ include/myVector/myVector/myVector.cpp
@@ -0,0 +1,12 @@
+﻿// myVector.cpp : Defines the entry point for the application.
+//
+
+#include "myVector.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
