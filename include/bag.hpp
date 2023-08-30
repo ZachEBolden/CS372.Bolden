feb27396080238f88590cc09d5b5bc9ff3f3688e
ZachEBolden <zachebolden@gmail.com>
2023-08-29 22:17:50 -0500

    bag updated but not done

--- include/bag.hpp
+++ include/bag.hpp
@@ -9,36 +9,70 @@
 #include "vector.hpp"
 
 template <typename Thing>
-class Bag {
+class Bag 
+{
 public:
-  void insert(Thing aThing) {
-    bagContents.push_back(aThing);
-    bagSize++;
-  }
-  Thing &pop() {
-    Thing aThing;
-    if (bagContents.size() > 0) {
-      aThing = bagContents[bagSize];
-      bagSize--;
+
+    ReceiptBag() : nextReceipt(1) {}//received help from Jo Gentry
+    void insert(Thing aThing) 
+    {
+        bagContents.push_back(aThing);
+        bagSize++;
     }
-    else {
-      std::cerr << "Can't pop out of an empty bag" << std::endl;
+
+    Thing& remove(int receipt)//received help from Jo Gentry
+    {
+        auto iter = std::find_if(bagVector.begin(), bagVector.end(),
+            [receipt](const Item& item) { return item.receipt == receipt; });
+
+        if (iter == bagVector.end())
+        {
+            throw std::runtime_error("This in not in the bag bro!");
+        }
+
+        Thing& removeItem = iter->value;
+        bagVector.erase(iter);
+        return removeItem;
     }
-    return aThing;
-  }
-  int size() {
-    return bagSize;
-  }
-  int count(Thing aThing) {
-    int bagCount = 0;
-    for (int i = 0 ; i < bagSize; i++) {
-      if (bagContents[i] == aThing) {
-        bagCount++;
-      }
+
+    //Thing &pop() 
+    //{
+    //    Thing aThing;
+    //   if (bagContents.size() > 0) 
+    //    {
+    //        aThing = bagContents[bagSize];
+    //        bagSize--;
+    //    }
+    //    else 
+    //   {
+    //       std::cerr << "Can't pop out of an empty bag" << std::endl;
+    //    }
+    //    return aThing;
+   // }
+    int size() 
+    {
+        return bagSize;
     }
-    return bagCount;
-  }
+    int count(Thing aThing) 
+    {
+        int bagCount = 0;
+        for (int i = 0 ; i < bagSize; i++) 
+        {
+            if (bagContents[i] == aThing) 
+            {
+                bagCount++;
+            }
+        }
+        return bagCount;
+    }
 private:
-  vector<Thing> bagContents;
-  int bagSize = 0;
+
+    struct Item
+    {
+        Thing value;
+        int receipt;
+    };
+    std::vector<Item> bagVector;
+    int bagSize = 0;
+    int nextReceipt;
 };
