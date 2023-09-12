531fe1cd25053fd3d8024e449ec4d5a083eb0992
ZachEBolden <zachebolden@gmail.com>
2023-09-12 15:39:13 -0500

    Hot Potato with weird error

---
+++ include/Josephus/Josephus.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include "Josephus.h"
+
+
+
+int main()
+{
+    int N, M;
+
+    std::cout << "Enter the number of people (N): ";
+    std::cin >> N;
+
+    std::cout << "Enter the number of passes (M): ";
+    std::cin >> M;
+
+    HotPotato<int> potato;
+    potato.createPotato(N);
+    potato.eliminatePeople(M);
+
+    int winningPosition = potato.getWinningPosition();
+
+    std::cout << "The winning position is: " << winningPosition << std::endl;
+
+    return 0;
+}
